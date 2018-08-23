e7257d21a0b760cc1529e3fc7e55fe960af946d5
Fengru <lfrharry@gmail.com>
2018-08-23 09:55:51 -0400

    Add RedEdge part

--- receiver.py
+++ receiver.py
@@ -5,44 +5,38 @@ import threading
 import signal
 import sys
 from socket import timeout
+import requests
+from io import open as iopen
 
 
 # import rospy
 from sensor_msgs.msg import LaserScan
+from cv_bridge import CvBridge
+from sensor_msgs.msg import CompressedImage
 
 
 
-def receive_left(UDP_PORT,timeout_limit):
+def receive_image(UDP_PORT,timeout_limit,name,msg):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	sock.bind((UDP_IP, UDP_PORT)) 
 	sock.settimeout(timeout_limit)
 	sockets_list.append(sock)
+	bridge = CvBridge()
 	while True:
 		try:
 			data = sock.recv(65536) 
-			np_arr = np.fromstring(data, np.uint8)
-			image_np = cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
-			cv2.imwrite('left.png',image_np)
-			print('Left Image saved')
+			if name == 'left' or name =='right':
+				image_data = CompressedImage()
+				image_data.deserialize(data)
+				image = bridge.compressed_imgmsg_to_cv2(image_data)
+				# np_arr = np.fromstring(data, np.uint8)
+				# image = cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
+			cv2.imwrite('{}.png'.format(name),image)
+			print('{} saved'.format(msg))
 		except timeout:
-			print('Error: Left Image Timeout')
+			print('Error: {} Timeout'.format(msg))
 			sys.exit(1)
 
-def receive_right(UDP_PORT,timeout_limit):
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-	sock.bind((UDP_IP, UDP_PORT)) 
-	sock.settimeout(timeout_limit)
-	sockets_list.append(sock)
-	while True:
-		try:
-			data = sock.recv(65536) 
-			np_arr = np.fromstring(data, np.uint8)
-			image_np = cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
-			cv2.imwrite('right.png',image_np)
-			print('Right Image saved')
-		except timeout:
-			print('Error: Right Image Timeout')
-			sys.exit(1)
 
 def receive_laser(UDP_PORT,timeout_limit):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
@@ -54,16 +48,34 @@ def receive_laser(UDP_PORT,timeout_limit):
 			data = sock.recv(65536)
 			laser_data = LaserScan()
 			laser_data.deserialize(data)
-			print('Laser Data Received\n')
+			np.save('laser',laser_data)
+			# print('Laser Data Received\n')
 		except timeout:
 			print('Error: Laser Data Timeout')
+			sys.exit(1)
+
+def receive_rededge(UDP_PORT,timeout_limit):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+	sock.bind((UDP_IP, UDP_PORT)) 
+	sock.settimeout(timeout_limit)
+	sockets_list.append(sock)
+	while True:
+		try:
+			data = sock.recv(65536)
+			file_names = data.split(";")
+			for name in file_names:
+				r = requests.get(name)
+				with iopen(name[-5:], 'wb') as file:
+					file.write(r.content)
+		except timeout:
+			print('Error: Rededge Timeout')
 			sys.exit(1)
 
 def handler(signum, frame):
     for s in sockets_list:
     	s.close()
-    for t in thread_list:
-    	t.close()
+    # for t in thread_list:
+    # 	t.close()
     sys.exit(0)
 
 def get_ip():
@@ -83,33 +95,45 @@ if __name__ == '__main__':
 
 	# get UDP parameters
 	UDP_IP = get_ip()
-	timeout_limit = 10.0
-	leftimage_port = 5005
-	rightimage_port = 5006
+	timeout_limit = 20.0
+	port_list = [5005,5006]
+	name_list = ['left','right']
+	msg_list = ['Left Image','Right Image']
 	laserscan_port = 5007
+	rededge_port = 5008
 
 	print('Timeout setting: {}s'.format(timeout_limit))
 	print('IP Address is: {}'.format(UDP_IP))
-	print('Left Image Port: {}'.format(leftimage_port))
-	print('Right Image Port: {}'.format(rightimage_port))
+	for cnt in range(len(port_list)):
+		print('{} Port: {}'.format(msg_list[cnt],port_list[cnt]))
 	print('Laser Scan Port: {}'.format(laserscan_port))
+	print('Rededge Port: {}'.format(rededge_port))
 
 	#start thread for receving
 	sockets_list = []
 	thread_list = []
 	signal.signal(signal.SIGINT, handler)
 	try:
-   		thread_list.append(threading.Thread(target=receive_left, args=(leftimage_port,timeout_limit)))
-   		thread_list.append(threading.Thread(target=receive_right, args=(rightimage_port,timeout_limit)))
-   		thread_list.append(threading.Thread(target=receive_laser,args=(laserscan_port,timeout_limit)))
-   		for t in thread_list:
-   			t.start()
-   		for t in thread_list:
-   			t.join()
-	except:
-   		pass
-   	finally:
-   		for s in sockets_list:
-   			s.close()
-
-
+		for cnt in range(len(port_list)):
+			thread_list.append(
+				threading.Thread(target=receive_image, 
+					args=(port_list[cnt],timeout_limit,name_list[cnt],msg_list[cnt])
+					)
+				)
+		thread_list.append(
+			threading.Thread(target=receive_laser,
+				args=(laserscan_port,timeout_limit)
+				)
+			)
+		thread_list.append(
+			threading.Thread(target=receive_rededge,
+				args=(rededge_port,timeout_limit)
+				)
+			)
+		for t in thread_list:
+			t.start()
+		for t in thread_list:
+			t.join()
+	finally:
+		for s in sockets_list:
+			s.close()
